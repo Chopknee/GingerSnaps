c3ac9153395465b1061dcba3aca086a16b03f9d1
Chopknee <chopknee@gmail.com>
2023-02-05 14:33:22 -0500

    Worked on character model script.

--- Assets/GingerSnaps/Scripts/Player/CharacterModel.cs
+++ Assets/GingerSnaps/Scripts/Player/CharacterModel.cs
@@ -10,14 +10,22 @@ namespace GingerSnaps.Player {
 		private Controller controller = null;
 
 		private void Awake() {
-			controller = GetComponent<Controller>();
 
 			if (model == null)
-				Debug.Log("Please assign the model in the editor!");
+				Debug.Log("Please assign the model in the editor!", gameObject);
+		}
+
+		private void Start() {
+			controller = GetComponent<Controller>();
 		}
 
 		private void Update() {
+			if (controller == null || model == null)
+				return;
 
+			if (controller.localVelocity.magnitude > 0.01f) {
+				model.forward = controller.localVelocity;
+			}
 		}
 
 	}
