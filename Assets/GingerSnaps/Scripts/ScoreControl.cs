dbde4baf6b0a10d8bd8f66a011fc6ac82d92b0c7
curtis <curtis>
2023-02-04 22:32:45 -0500

    status screen

--- Assets/GingerSnaps/Scripts/ScoreControl.cs
+++ Assets/GingerSnaps/Scripts/ScoreControl.cs
@@ -7,6 +7,10 @@ public class ScoreControl : MonoBehaviour
 {
     public float secRemain = 10f;
     public float score = 0f;
+    private float finalScore = -1f;
+    private float displayScore = 0;
+
+    public GameObject status;
     
     // Start is called before the first frame update
     void Start()
@@ -18,9 +22,22 @@ public class ScoreControl : MonoBehaviour
     void Update()
     {
         secRemain -= Time.deltaTime;
-        if (secRemain <= 0){
-                secRemain = 0;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if (secRemain <= 0f){
+            if (finalScore == -1){
+                finalScore = score;
+            }
+                ///secRemain = 0;
+                status.SetActive(true);
+                if (displayScore < finalScore){
+                    displayScore ++;
+                }
+
+                status.GetComponent<TMPro.TextMeshProUGUI>().text = "Final Score: " + displayScore.ToString();
+              if (secRemain <= -10f)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }  
+        
         }
         
     }
